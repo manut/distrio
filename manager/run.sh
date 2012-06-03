b47fc84f5d0ec01f103f99d8e4dcb27a24465de6
Manuel Traut <manut@mecka.net>
2012-06-03 02:41:09 +0200

    manager: add run.sh to ease start of manager
    - checks if a tao naming service is running
    - starts the tao naming service if needed
    - starts distrio_manager with corba parameters to resolve naming server
    
    Signed-off-by: Manuel Traut <manut@mecka.net>

---
+++ manager/run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# startup script for distrio manager
+#
+# checks if a naming service is running
+#  if not; a newone is started before distrio_manager
+#
+# author: Manuel Traut <manut@mecka.net>
+
+NS_PORT=12345
+
+NS=`ps aux | grep tao_cosnaming | grep $NS_PORT`
+
+if [ -z "$NS" ]; then
+	echo start naming service
+	tao_cosnaming -ORBEndPoint iiop://localhost:$NS_PORT &
+	sleep 1
+fi
+
+LD_LIBRARY_PATH=../io:../common ./distrio_manager \
+	-ORBInitRef NameService=corbaloc:iiop:localhost:12345/NameService \
+	$@
