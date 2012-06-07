f5e17d19e473e501ffa964ec3ccda1970ecafbf2
Manuel Traut <manut@mecka.net>
2012-06-07 13:58:49 +0200

    simple_dev: fixup paths in run.sh
    - according to changed project structure
    
    Signed-off-by: Manuel Traut <manut@mecka.net>

--- devices/simple_dev/run.sh
+++ devices/simple_dev/run.sh
@@ -4,6 +4,7 @@
 #
 # author: Manuel Traut <manut@mecka.net>
 
-LD_LIBRARY_PATH=../../io:../../common:../../manager/lib ./distrio_simple_dev \
+LD_LIBRARY_PATH=../../io/lib:../../common:../../manager/lib \
+	./distrio_simple_dev \
 	-ORBInitRef NameService=corbaloc:iiop:localhost:12345/NameService \
 	$@
