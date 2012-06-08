bdf4c406cd80fe0e25df68ef98a8b626093290c4
Manuel Traut <manut@mecka.net>
2012-06-08 03:47:25 +0200

    generic_gpio: add io simulator
    - used for testing of callbacks to get events without real hw
    
    Signed-off-by: Manuel Traut <manut@mecka.net>

--- io/bin/generic_gpio/generic_gpio.cpp
+++ io/bin/generic_gpio/generic_gpio.cpp
@@ -7,32 +7,46 @@
 
 #include <iostream>
 
-class Io_manager : public  ACE_Task < ACE_MT_SYNCH > {
-	public:
-		Io_manager () { }
-		~Io_manager () { }
-
-	private:
-		int svc (void) {
-			while (1) {
-				/** TODO: implement some cyclic gpio stuff */
-				sleep (1);
-			}
-		}
-};
-
 class My_digital : public Distrio_Digital_i {
 	public:
 		My_digital (std::string io_name) : Distrio_Digital_i (io_name) { }
 
 		::Distrio::Error *set () {
+			update_timestamp ();
+			val = 1;
 			std::cout << "set" << std::endl;
 			return distrio_success ();
 		}
 
 		::Distrio::Error *reset () {
+			update_timestamp ();
+			val = 0;
 			std::cout << "reset" << std::endl;
 			return distrio_success ();
+		}
+
+		::Distrio::Error *get (::CORBA::Long_out value) {
+			value = val;
+			std::cout << "get" << std::endl;
+			return distrio_success ();
+		}
+};
+
+class Io_simulator : public  ACE_Task < ACE_MT_SYNCH > {
+	public:
+		Io_simulator (My_digital *_io) { io = _io; }
+		~Io_simulator () { }
+
+	private:
+		My_digital *io;
+
+		int svc (void) {
+			while (1) {
+				io->raise ();
+				sleep (3);
+				io->fall ();
+				sleep (5);
+			}
 		}
 };
 
@@ -40,7 +54,7 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 {
 	int ret = 0;
 	My_digital *digital;
-	/* Io_manager manager; */
+	Io_simulator *sim;
 
 	if (init_corba (argc, argv))
 		return -EINVAL;
@@ -55,13 +69,11 @@ ACE_TMAIN (int argc, ACE_TCHAR *argv[])
 		goto out;
 	}
 
+	sim = new Io_simulator (digital);
+
 	std::cout << "registered id: " << digital->id () << std::endl;
 
-	/* TODO: activate if cyclic stuff is needed
-	manager.activate ();
-	manager.wait ();
-	*/
-
+	sim->activate ();
 	join_orb ();
 
 out:
